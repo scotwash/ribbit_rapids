7a3621745263e5dc96fa7678641a0adc534c3a43
milanwashington54@gmail.com <milanwashington54@gmail.com>
2025-02-13 19:08:10 -0500

    Initial commit

---
+++ scripts/menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+# starts game, leads to main map 
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/main_screen.tscn")
+	pass # Replace with function body.
+
+# when this button is pressed it takes the player to the credits scene 
+func _on_credits_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/credits.tscn")
+	pass
+
+# when this button is pressed the game exits out of itself
+func _on_quit_button_pressed():
+	get_tree().quit()
+	pass # Replace with function body.
