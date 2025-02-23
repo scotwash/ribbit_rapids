45a481e6cb4cb72f83a7a0e8bd4dc3daf9eab4e8
milanwashington54@gmail.com <milanwashington54@gmail.com>
2025-02-23 13:31:30 -0500

    organized the folder

--- scripts/cards.gd
+++ scripts/cards.gd
@@ -1,15 +1,1 @@
 extends Node2D
-
-var grassCard = load("res://tilesets/cards/grass3.png")
-var lilyPadCard = load("res://tilesets/cards/Lilypad_10.png")
-var waterCard = load("res://tilesets/cards/Woter_10.png")
-var matches = 0 
-
-
-
-
-func _process(delta):
-	pass
-	
-func _ready():
-	pass
